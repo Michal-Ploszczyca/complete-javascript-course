4502a61036464f963803ea053c0cb4dd9efb3052
michalploszczyca <michal.ploszczyca98@gmail.com>
2023-11-20 16:10:04 +0100

    lesson 13

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,5 +1,39 @@
-let country = 'Poland'
-let continent = "Europe"
+/*
+// assignment 1
+
+// let country = 'Poland'
+// let continent = "Europe"
 let population = 39000000
 
-console.log(country, continent, population)
+// console.log(country, continent, population)
+
+// asignment 2
+
+let isIsland = false;
+let language = null
+
+console.log(typeof isIsland)
+console.log(typeof language)
+console.log(typeof population)
+
+
+// let const var exercise
+
+const language = 'Polish';
+console.log(typeof language)
+
+
+const population = 39000000 
+const country = 'Poland'
+const language = 'Polish'
+
+const description = 
+country + 
+" is in Europe, and its " + 
+population + 
+" million people speak " + 
+language;
+
+console.log(description)
+*/
+
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,8 +1,6 @@
+/*
 let js = 'AMAZING'
 console.log(1 + 1 + " kebaby NA cienkim");
-
-
-
 
 let first_name = "Matilda";
 
@@ -10,3 +8,53 @@ console.log(first_name);
 
 let myFirstJob = "Programmer";
 console.log(myFirstJob)
+
+
+// console.log(javascriptIsFun);
+let javascriptIsFun = true;
+// console.log(javascriptIsFun)
+
+console.log(typeof javascriptIsFun)
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun)
+
+let year;
+console.log(year)
+
+year = 1991;
+console.log(typeof year);
+
+
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+
+*/
+// math operators
+const currentYear = 2023;
+const ageMichal = currentYear - 1998;
+const ageKaczynski = currentYear - 1949;
+console.log(ageMichal * 2, ageKaczynski / 2, ageKaczynski ** 2)
+
+const first_name = 'Jonas';
+const lastName = 'Shmedtman'
+console.log(first_name + ' ' +  lastName)
+
+// asignment operators
+let x = 10 + 5; // 15
+x += 15; // x = x + 15  = 30
+x++; // x = x + 1
+x--; // x= x -1
+console.log(x)
+
+//comparision operators
+
+console.log(ageMichal > ageKaczynski); // >, < >=, <=
+
+console.log(ageMichal >= 18)
+
+const isFullAge = ageKaczynski >= 18;
+console.log()
