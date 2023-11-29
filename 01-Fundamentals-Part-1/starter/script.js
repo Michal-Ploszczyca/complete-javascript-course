f526046bc0fe9794b91477f4af984b6e59024f05
michalploszczyca <michal.ploszczyca98@gmail.com>
2023-11-29 20:53:43 +0100

    update

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -79,7 +79,6 @@ const markHigherBMI = markBMi > johnBMI;
 console.log("Does Mark has a higher BMI than John? The answer is: " + markHigherBMI);
 
 
-*/
 
 const first_name = 'Jonas';
 const job = "teacher";
@@ -98,3 +97,34 @@ const color = "kurwa zielony XD"
 
 const michalNew = `Siema jestem Michał i mój numer telefonu to ${phone} a obudowa ma kolor ${color}`
 console.log(michalNew)
+
+
+console.log("String with \n\multiple lines ");
+
+console.log(`string 
+done as
+templeta 
+litereal`);
+
+const sentence = `My name is ${first_name} and i am a ${job}, my father was born in ${birthYear} 
+and i was born in ${year}, my head color is ${color}`
+console.log(sentence)
+
+*/
+
+const age = 17;
+const is_old_enough = age >= 18;
+
+if (is_old_enough) {
+    console.log("Sarah can start her driving license 🚌")
+    
+    
+}  else {
+
+    const years_left = 18 - age
+    const spelling = years_left % 2 != 1
+    
+    console.log(`Sarah has to wait ${years_left} years till she can start her driving license `)
+
+}
+
