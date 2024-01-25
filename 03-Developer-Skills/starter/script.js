cb4e6369cc47e39357d5bab9fb329ef0a05f11fe
michalploszczyca <michal.ploszczyca98@gmail.com>
2024-01-25 17:28:17 +0100

    chapter 3

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,10 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = 23;
+
+const calcAge = birthYeara => 2037 - birthYeara;
+
+if (x === 23) console.log(23);
+
+console.log(calcAge(1991));
