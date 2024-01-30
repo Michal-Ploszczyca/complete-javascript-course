09552ef9d94bdcad9404f109e4d8c2e340f2f31c
michalploszczyca <michal.ploszczyca98@gmail.com>
2024-01-30 22:59:42 +0100

    lesson stackoverflow

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,6 +1,6 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
-
+/*
 const x = 23;
 
 const calcAge = birthYeara => 2037 - birthYeara;
@@ -8,3 +8,43 @@ const calcAge = birthYeara => 2037 - birthYeara;
 if (x === 23) console.log(23);
 
 console.log(calcAge(1991));
+*/
+const temperatures = [3, -2, 4, -1, 5, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+const CalcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let index = 0; index < temps.length; index++) {
+    const curTemp = temps[index];
+    if (typeof curTemp !== 'number') continue;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const aplitude = CalcTempAmplitude(temperatures);
+console.log(aplitude);
+
+// MERGE two arrays
+
+const CalcTempAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let index = 0; index < temps.length; index++) {
+    const curTemp = temps[index];
+    if (typeof curTemp !== 'number') continue;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const aplitudeNew = CalcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(aplitudeNew);
